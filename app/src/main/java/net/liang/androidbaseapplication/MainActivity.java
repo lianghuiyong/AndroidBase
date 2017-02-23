12aaa618b8c830b8bc04109cf2305a853fa1fc9f
梁惠涌 <lianghuiyong@outlook.com>
2017-02-23 16:48:04 +0800

    up

--- app/src/main/java/net/liang/androidbaseapplication/MainActivity.java
+++ app/src/main/java/net/liang/androidbaseapplication/MainActivity.java
@@ -2,8 +2,9 @@ package net.liang.androidbaseapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import com.socks.library.KLog;
 
 import net.liang.appbaselibrary.base.BaseAppCompatActivity;
 import net.liang.appbaselibrary.base.mvp.MvpPresenter;
@@ -25,23 +26,35 @@ public class MainActivity extends BaseAppCompatActivity {
 
     @Override
     public void init() {
-        setToolbarCentel_tv(true,"测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试","哈哈");
+        setToolbarCentel_tv(true, "测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试", "哈哈");
     }
 
     @OnClick({R.id.baseRecyclerViewActivity, R.id.baseRecyclerViewFragment})
     public void onClick(View view) {
         Intent intent = new Intent();
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.baseRecyclerViewActivity:
-                intent.setClass(this,Test_BaseRecyclerViewActivity.class);
+                intent.setClass(this, Test_BaseRecyclerViewActivity.class);
                 startActivity(intent);
                 break;
 
             case R.id.baseRecyclerViewFragment:
-                intent.setClass(this,Test_BaseRecyclerViewFragment.class);
+                intent.setClass(this, Test_BaseRecyclerViewFragment.class);
                 startActivity(intent);
                 break;
         }
 
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.right_tv)
+    public void onClick() {
+        KLog.e("55555555555");
     }
 }
